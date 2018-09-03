8cd2c1818422fe3c829d6d49f6d67eaf1bcdb36d
Richard sikra <richiesikra@gmail.com>
2018-09-03 13:14:04 -0700

    Started organizing code

--- devrichie.js
+++ devrichie.js
@@ -5,51 +5,91 @@ const fs = require('fs');
 const hostname = 'localhost';
 const port = 8080;
 
+
+
 const options       = require(__dirname + "/assets/json/options.json",  'utf8');
 const projectsJson  = require(__dirname + "/assets/json/projects.json", 'utf8');
 const toolsJson     = require(__dirname + "/assets/json/tools.json",    'utf8');
 
-projectsJson.forEach(project => {
+
+
+
+
+
+
+
+
+
+var processController = {
+
+  allTools: [],
+  normalTools: [],
+  featuredTools: [],
+  projectsJson: [],
+  toolsJson: [],
+  options: [],
+
+  run: function(options, projectsJson, toolsJson){
+
+    var self          = this;
+
+    self.options      = options;
+    self.toolsJson    = toolsJson;
+    self.projectsJson = projectsJson;
+
+    self.projectsJson.forEach(project => {
       
-  project.tools = [];
-
-});
-
-var allTools = [];
-var normalTools = [];
-var featuredTools = [];
-
-toolsJson.forEach(tool => {
-
-  var projectSlugs = tool.projects;
-  var toolsProjects = [];
-
-  projectSlugs.forEach(projectSlug => {
-    projectsJson.forEach(project => {
-
-      if(project.slug === projectSlug){
-        toolsProjects.push(project);
-        project.tools.push(tool);
-      }
-
+      project.tools = [];
+    
     });
-  });
+    
+    self.toolsJson.forEach(tool => {
+    
+      var projectSlugs = tool.projects;
+      var toolsProjects = [];
+    
+      projectSlugs.forEach(projectSlug => {
+        projectsJson.forEach(project => {
+    
+          if(project.slug === projectSlug){
+            toolsProjects.push(project);
+            project.tools.push(tool);
+          }
+    
+        });
+      });
+    
+      tool.projects = toolsProjects;
+      
+      if(tool.featured === false){
+        self.normalTools.push(tool);
+      }else{
+        self.featuredTools.push(tool);
+      }
+    
+    });
+    
+    self.allTools = self.featuredTools.concat(self.normalTools);
 
-  tool.projects = toolsProjects;
-  
-  if(tool.featured === false){
-    normalTools.push(tool);
-  }else{
-    featuredTools.push(tool);
+    self.allTools.sort     ((a, b) => a.order - b.order);
+    self.normalTools.sort  ((a, b) => a.order - b.order);
+    self.featuredTools.sort((a, b) => a.order - b.order);
+
   }
+};
 
-});
 
-allTools = featuredTools.concat(normalTools);
+processController.run(options, projectsJson, toolsJson);
 
-allTools.sort     ((a, b) => a.order - b.order);
-normalTools.sort  ((a, b) => a.order - b.order);
-featuredTools.sort((a, b) => a.order - b.order);
+
+
+
+
+
+
+
+
+
 
 const server = http.createServer((req, res) => {
 
@@ -58,43 +98,43 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
     const compiledPug = pug.renderFile(__dirname + "/index.pug", {
-      options: options,
-      tools: allTools,
-      projects: projectsJson,
-      featuredTools: featuredTools,
-      normalTools: normalTools
+      options: processController.options,
+      tools: processController.allTools,
+      projects: processController.projectsJson,
+      featuredTools: processController.featuredTools,
+      normalTools: processController.normalTools
     });
     res.end(compiledPug);
   }
 
   // Create page for every project (example.com/projects/slug and example.com/projects/slug/)
-  projectsJson.forEach(project => {
+  processController.projectsJson.forEach(project => {
     
     const projectUrl = {
       "path": ("/projects/" + project.slug),
       "imagePath" : ("/projects/" + project.slug) + "/images/"
     }
 
-    if(project.banners){
-      project.banners.forEach(projectBanner =>{        
+    if(project.images){
+      project.images.forEach(image =>{        
         // ex url: www/projects/lightshow/images/banner-225x150
         //         www/projects/lightshow/images/banner-225x150/
 
-        const bannerUrl = {
-          "url" : (projectUrl.imagePath + projectBanner.slug), 
-          "path": (__dirname + "/assets/projects/" + project.slug + "/images/" + projectBanner.path)
+        const imageUrl = {
+          "url" : (projectUrl.imagePath + image.slug), 
+          "path": (__dirname + "/assets/projects/" + project.slug + "/images/" + image.path)
         };
 
-        if (req.url === (bannerUrl.url) || 
-            req.url === (bannerUrl.url) + "/"){
+        if (req.url === (imageUrl.url) || 
+            req.url === (imageUrl.url) + "/"){
 
-          fs.readFile(bannerUrl.path, function(err, data) {
+          fs.readFile(imageUrl.path, function(err, data) {
             if(err){
               res.writeHead(404);
               res.write("Not Found!");
             }
             else{
-              res.writeHead(200, {'Content-Type': projectBanner.mime});
+              res.writeHead(200, {'Content-Type': image.mime});
               res.write(data);
             }
             res.end();
@@ -111,8 +151,8 @@ const server = http.createServer((req, res) => {
       res.statusCode = 200;
       res.setHeader("Content-Type", "text/html");
       const compiledPug = pug.renderFile(__dirname + "/single-project.pug", {
-        options: options,
-        projects: projectsJson,
+        options: processController.options,
+        projects: processController.projectsJson,
         project: project
       });
       
@@ -123,19 +163,19 @@ const server = http.createServer((req, res) => {
   
 
   // Create page for every tool (example.com/tools/slug and example.com/tools/slug/)
-  toolsJson.forEach(element => {
+  processController.toolsJson.forEach(tool => {
 
-    if (req.url === ("/tools/" + element.slug) || 
-        req.url === ("/tools/" + element.slug) + "/"){
+    if (req.url === ("/tools/" + tool.slug) || 
+        req.url === ("/tools/" + tool.slug) + "/"){
 
         res.statusCode = 200;
         res.setHeader("Content-Type", "text/html");
         const compiledPug = pug.renderFile(__dirname + "/single-tool.pug", {
-          options: options,
-          tools: allTools,
-          tool: element,
-          featuredTools: featuredTools,
-          normalTools: normalTools
+          options: processController.options,
+          tools: processController.allTools,
+          tool: tool,
+          featuredTools: processController.featuredTools,
+          normalTools: processController.normalTools
         });
         
         res.end(compiledPug);          
@@ -147,8 +187,8 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
     const compiledPug = pug.renderFile(__dirname + "/projects.pug", {
-      options: options,
-      projects: projectsJson
+      options: processController.options,
+      projects: processController.projectsJson
     });
     res.end(compiledPug);
   }
@@ -158,10 +198,10 @@ const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
     const compiledPug = pug.renderFile(__dirname + "/tools.pug", {
-      options: options,
-      tools: allTools,
-      featuredTools: featuredTools,
-      normalTools: normalTools
+      options: processController.options,
+      tools: processController.allTools,
+      featuredTools: processController.featuredTools,
+      normalTools: processController.normalTools
     });
     res.end(compiledPug);
   }
